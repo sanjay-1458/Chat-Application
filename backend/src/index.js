11edb3bc5385f56d7ac9cfbdd3a7de93f4b57478
Sanjay <121040100+sanjay-1458@users.noreply.github.com>
2025-03-21 16:12:11 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -17,8 +17,8 @@ dotenv.config();
 const PORT = process.env.PORT;
 const __dirname = path.resolve();
 
-app.use(express.json({ limit: "10mb" })); 
-app.use(express.urlencoded({ limit: "10mb", extended: true }));
+app.use(express.json({ limit: "5mb" })); 
+app.use(express.urlencoded({ limit: "5mb", extended: true }));
 
 app.use(cookieParser());
 app.use(
